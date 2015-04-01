3e805644791a281aa85aa89d4278a98c0e578095
Michael Allan <michaelallan@optushome.com.au>
2015-04-02 10:39:39 +1100

    Push to test CI setup

--- app/routes/index.js
+++ app/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'JobFinder and CI' });
+  res.render('index', { title: 'JobFinder' });
 });
 
 module.exports = router;

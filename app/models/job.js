c034838f19bfbd188ad53d3f33d585359f437fd7
Michael Allan <michaelallan@optushome.com.au>
2015-04-01 18:10:32 +1100

    Test db is seeded with values if empty

--- app/models/job.js
+++ app/models/job.js
@@ -8,14 +8,14 @@ var jobSchema = mongoose.Schema({
 var Job = mongoose.model('Job', jobSchema);
 
 // NOTE export this seed function until we've added POSTing capability to the api
-function seedJobs() {
+function seedJobs(callback) {
   Job.find({})
     .exec(function (err, collection) {
       if (collection.length === 0) {
         Job.create({ title: 'Cook', description: 'You will be making bagels' });
         Job.create({ title: 'Waiter', description: 'You will be putting food on tables' });
         Job.create({ title: 'Programmer', description: 'You will be typing, googling and arguing' });
-        Job.create({ title: 'Blacksmith', description: 'You will be making horse shoes and axe heads' });
+        Job.create({ title: 'Blacksmith', description: 'You will be making horse shoes and axe heads' }, callback);
       }
     });
 };

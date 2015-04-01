585fb854362870246166aa61db394d1619347793
Michael Allan <michaelallan@optushome.com.au>
2015-04-01 15:08:17 +1100

    Return jobs from mongo

--- app/routes/api.js
+++ app/routes/api.js
@@ -1,9 +1,22 @@
 var express = require('express');
 var router = express.Router();
 
+var mongoose = require('mongoose');
 
+// Heartbeat
 router.get('/', function (req, res, next) {
-  res.send('respond with a list of all jobs');
+  res.sendStatus(200);
+});
+
+// Jobs API
+router.get('/jobs', function (req, res, next) {
+  mongoose.model('Job')
+    .find({})
+    .exec(function (err, collection) {
+      if (err) return next(err);
+
+      res.json(collection);
+    })
 });
 
 module.exports = router;

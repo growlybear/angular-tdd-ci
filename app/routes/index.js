bc96f3520ef3348cd6f790779c4c1da217d5d563
Michael Allan <michaelallan@optushome.com.au>
2015-04-02 11:22:47 +1100

    Temp text change

--- app/routes/index.js
+++ app/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'JobFinder' });
+  res.render('index', { title: 'JobFinder again' });
 });
 
 module.exports = router;
